96312ee0cef7cec3a7fe29cbbd06077781b5d2ee
Michael C. Stroh <stroh@astroh.org>
2023-05-30 16:43:11 -0500

    Minor tweaks.

--- default.sh
+++ default.sh
@@ -271,4 +271,6 @@ function fix_permissions {
     chmod -R go+rX /projects/b1094/software/miniconda
     chmod -R go-w /projects/b1094/software/environments
     chmod -R go+rX /projects/b1094/software/environments
+    chmod -R go-w /projects/b1094/software/miniforge3
+    chmod -R go+rX /projects/b1094/software/miniforge3
 }
