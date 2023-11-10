f78fe7a6160bf711eb8eb7def07877cc6bbbe174
Michael C. Stroh <stroh@astroh.org>
2023-11-10 12:51:55 -0600

    Added macOS paths for FTOOLS and CALDB.

--- basics.sh
+++ basics.sh
@@ -151,3 +151,21 @@ fi
 #
 #
 ###############################################################################
+
+###############################################################################
+#
+# FTOOLS/CALDB
+#
+if [ "$(uname)"=="Darwin" ] && [ -d "/Users/data/heasoft" ] ; then
+    export HEADAS=/Users/data/heasoft/x86_64-apple-darwin22.6.0
+    . $HEADAS/headas-init.sh
+fi
+if [ "$(uname)"=="Darwin" ] && [ -d "/Users/data/caldb" ] ; then
+    export CALDB=/Users/data/caldb
+    . $CALDB/software/tools/caldbinit.sh
+fi
+#
+#
+###############################################################################
+
+
