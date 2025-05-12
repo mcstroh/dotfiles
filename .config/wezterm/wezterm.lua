ea64caeb891de2fdfb37afc638058535f1f19b37
mstroh <mstroh@tremors.aoc.nrao.edu>
2025-05-12 08:10:20 -0600

    Fixed font_with_fallback call.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,7 +3,7 @@ local config = wezterm.config_builder()
 
 config.color_scheme = "Argonaut"
 
-config.font = wezterm.font("MesloLGS NF")
+config.font = wezterm.font_with_fallback {'MesloLGS Nerd Font', 'MesloLGS NF'}
 config.font_size = 11
 
 config.hide_tab_bar_if_only_one_tab = true
