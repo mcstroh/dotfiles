366069ae4202852e9fc8d967422708e4aea53fcb
Michael Stroh <github@michaelcstroh.com>
2025-05-28 10:15:23 -0600

    Split wezterm configuration files.

---
+++ .config/wezterm/wezterm-linux.lua
@@ -0,0 +1,18 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+config.color_scheme = "Argonaut"
+
+config.font = wezterm.font("MesloLGS Nerd Font")
+config.font_size = 11
+
+config.hide_tab_bar_if_only_one_tab = true
+ -- config.enable_tab_bar = false
+
+ -- config.window_decorations = "RESIZE"
+
+config.window_background_opacity = 0.8
+config.macos_window_background_blur = 10
+config.adjust_window_size_when_changing_font_size = false
+
+return config
---
+++ .config/wezterm/wezterm-macos.lua
@@ -0,0 +1,20 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+config.color_scheme = "Argonaut"
+
+config.font = wezterm.font_with_fallback {'MesloLGS Nerd Font', 'MesloLGS NF'}
+config.font_size = 11
+
+config.hide_tab_bar_if_only_one_tab = true
+ -- config.enable_tab_bar = false
+
+config.window_decorations = "RESIZE"
+
+config.window_background_opacity = 0.8
+config.macos_window_background_blur = 10
+config.adjust_window_size_when_changing_font_size = false
+
+config.front_end = "WebGpu"
+
+return config
